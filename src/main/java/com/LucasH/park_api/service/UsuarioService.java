3a809878f453e97b4e3f55678c38f2a4ef411d92
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-30 23:31:18 -0300

    add service and usuario controller

---
+++ src/main/java/com/LucasH/park_api/service/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.LucasH.park_api.service;
+
+import com.LucasH.park_api.entity.Usuario;
+import com.LucasH.park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+}
