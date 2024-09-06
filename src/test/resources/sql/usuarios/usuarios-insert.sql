94627797d3fc50f33f4d7f642772ee904bec1451
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-09-06 20:43:22 -0300

    adicionando testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS  (id, username, password, role) values (100, 'test@gmail.com', '123456', 'ADMIN')
+insert into USUARIOS  (id, username, password, role) values (101, 'bob@gmail.com', '123456', 'USER')
+insert into USUARIOS  (id, username, password, role) values (102, 'raissa@gmail.com', '123456', 'USER')
