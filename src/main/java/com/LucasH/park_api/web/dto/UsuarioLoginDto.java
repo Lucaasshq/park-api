3f4f85feec463eb6996dffbe1ccd6ec9eab6210f
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-09-11 16:31:58 -0300

    criando me´todo de autenticação e UsuarioLoginDto

---
+++ src/main/java/com/LucasH/park_api/web/dto/UsuarioLoginDto.java
@@ -0,0 +1,22 @@
+package com.LucasH.park_api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UsuarioLoginDto {
+
+    @NotBlank
+    @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$" ,message = "formato do e-mail está invalido")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
