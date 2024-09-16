dcfdd8d00e3d32233a48ba870c955cffa6b4c40c
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-09-15 23:17:42 -0300

    .

--- src/main/java/com/LucasH/park_api/jwt/JwtUserDetailsService.java
+++ src/main/java/com/LucasH/park_api/jwt/JwtUserDetailsService.java
@@ -1,7 +1,6 @@
 package com.LucasH.park_api.jwt;
 
 import com.LucasH.park_api.entity.Usuario;
-import com.LucasH.park_api.repository.UsuarioRepository;
 import com.LucasH.park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,9 +17,16 @@ public class JwtUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Implementa o método da interface UserDetailsService, que é usado pelo Spring Security para
+        // carregar os detalhes de um usuário com base no nome de usuário.
         Usuario usuario = usuarioService.buscarPorUsername(username);
+        // usa o usarioService para buscar um usuário no banco de dados
+        // a partir do username
 
         return new JwtUserDetails(usuario);
+        //Se o usuário for encontrado, é retornada uma instância de JwtUserDetails,
+        // que é uma classe que estende User e implementa a interface UserDetails.
+        // O Spring Security utiliza este objeto UserDetails para autenticar o usuário
     }
 
 
@@ -28,6 +34,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     public JwtToken getTokenAuthenticated(String username) {
      Usuario.Role role  = usuarioService.buscarRolePorUsername(username);
          return JwtUtils.createJwtToken(username, role.name() );
+         //Gera um token JWT para um usuário autenticado, baseado no seu nome de usuário e papel (role).
 
     }
 }
