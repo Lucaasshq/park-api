11ebdd37956c5277cb5f599fd7060e05667e4ee1
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-09-22 18:18:58 -0300

    ClienteResponseDto

---
+++ src/main/java/com/LucasH/park_api/web/dto/ClienteResponseDto.java
@@ -0,0 +1,17 @@
+package com.LucasH.park_api.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ClienteResponseDto {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+}
