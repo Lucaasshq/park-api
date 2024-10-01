77ffc14841e33e1c7abbe706e254efc9291e456b
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-10-01 14:57:05 -0300

    incluido documentação do SWAGGER e  inciando testre end2end

--- src/main/java/com/LucasH/park_api/web/controller/EstacionamentoController.java
+++ src/main/java/com/LucasH/park_api/web/controller/EstacionamentoController.java
@@ -4,9 +4,18 @@ import com.LucasH.park_api.entity.ClienteVaga;
 import com.LucasH.park_api.service.EstacionamentoService;
 import com.LucasH.park_api.web.dto.EstacionamentoCreateDto;
 import com.LucasH.park_api.web.dto.EstacionamentoResponseDto;
+import com.LucasH.park_api.web.dto.UsuarioResponseDto;
 import com.LucasH.park_api.web.dto.mapper.ClienteVagaMapper;
+import com.LucasH.park_api.web.exeception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-
+@Tag(name = "Estacionamentos", description = "Operação de registro de entrada e saida de um veiculo do estacionamento.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/estacionamentos")
@@ -24,6 +33,22 @@ public class EstacionamentoController {
 
     private final EstacionamentoService estacionamentoService;
 
+
+    @Operation(summary = "Operação de check-in", description = "Recurso para dar entradde um veiculo no estacionamento",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Autenticação realizada com sucesso e retorno de um bearer token",
+                            headers = @Header(name = HttpHeaders.LOCATION,
+                            description = "URL de acesso ao recurso criado"),
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = EstacionamentoResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Causas possiveis: <br/>" +
+                            "- CPF do cliente não cadastrado no sistema <br/>" +
+                            "- Nenhuma vaga livre foi localizada;",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                @ApiResponse(responseCode = "403", description = "Recurso não permitdo ao ROLE= USER",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            })
     @PostMapping("/check-in")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<EstacionamentoResponseDto> checkIn (@RequestBody @Valid EstacionamentoCreateDto dto) {
--- src/main/java/com/LucasH/park_api/web/controller/VagaController.java
+++ src/main/java/com/LucasH/park_api/web/controller/VagaController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
@@ -24,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-
+@Tag(name = "Vagas")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/vagas")
